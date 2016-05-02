a2a125ab5f78db12b1787823e3edf2a0c194a150
christian-ericksen <christian.ericksen@gmail.com>
2016-05-02 17:02:14 -0400

    Second commit

--- app/src/main/java/Chamber.java
+++ app/src/main/java/Chamber.java
@@ -1,7 +1,17 @@
+import android.widget.ImageView;
+
 /**
  * Created by Christian on 4/29/16.
  */
 public class Chamber {
+
+    ImageView chamber1;
+    ImageView chamber2;
+    ImageView chamber3;
+    ImageView chamber4;
+    ImageView chamber5;
+    ImageView chamber6;
+    
     public Chamber(Boolean isFull, Boolean isLoaded) {
 
     }
