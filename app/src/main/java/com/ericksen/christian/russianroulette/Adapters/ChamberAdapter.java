f44df8024339a1f47c924f7028e53d5a4f973ccf
mrtmmn <mrtmmn@gmail.com>
2016-05-15 09:40:46 -0400

    Fit Everything Into Main Activity

--- app/src/main/java/com/ericksen/christian/russianroulette/Adapters/ChamberAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.ericksen.christian.russianroulette.Adapters;
-
-/**
- * Created by maratmamin on 5/14/16.
- */
-public class ChamberAdapter {
-}
--- app/src/main/java/com/ericksen/christian/russianroulette/Classes/Chamber.java
+++
@@ -1,7 +0,0 @@
-package com.ericksen.christian.russianroulette.Classes;
-
-/**
- * Created by maratmamin on 5/14/16.
- */
-public class Chamber {
-}
